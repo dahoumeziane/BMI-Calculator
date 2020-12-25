e31bcc4844bc591664c546b4996ce22f74ae72eb
Meziane Dahou <hd_meziane@esi.dz>
2020-12-25 16:27:38 +0100

    first commit

---
+++ BMI Calculator/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Dahou Meziane on 12/25/20.
+//  Copyright © 2020 Meziane Dahou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
