f560c59df3d600cc5fb57a33ae330bf38c618a40
Meziane Dahou <hd_meziane@esi.dz>
2021-02-26 19:32:21 +0100

    pass bmi value added

--- BMI Calculator/Controller/CalculateViewController.swift
+++ BMI Calculator/Controller/CalculateViewController.swift
@@ -14,6 +14,7 @@ class CalculateViewController: UIViewController {
     @IBOutlet weak var weightProgress: UISlider!
     @IBOutlet weak var heightText: UILabel!
     @IBOutlet weak var weightText: UILabel!
+    var bmiValue = "0.0"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -26,12 +27,22 @@ class CalculateViewController: UIViewController {
     @IBAction func weightChanged(_ sender: UISlider) {
         weightText.text = String(Int(sender.value))+"kg"
     }
+    
     @IBAction func calculatePressed(_ sender: UIButton) {
         let height = heightProgress.value
         let weight = weightProgress.value
-        print (String(weight/pow(height, 2)))
+        bmiValue = String(format: "%.2f", weight/pow(height, 2))
+        print (bmiValue)
         self.performSegue(withIdentifier: "goToResult", sender: self)
         
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult"{
+            let destinationVc = segue.destination as! ResultViewController
+            // we always need to set the destination uicontroller
+            // Downcasting
+            destinationVc.bmiValue = bmiValue
+        }
     }
 }
 
--- BMI Calculator/View/ResultViewController.swift
+++ BMI Calculator/View/ResultViewController.swift
@@ -9,9 +9,15 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
+    var bmiValue: String?
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var recalculateLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        bmiLabel.text = bmiValue
 
         // Do any additional setup after loading the view.
     }
